de1db9aa24ed74ca5793f12a8a0dd00bcac09277
Preston Timmons <prestontimmons@gfa.org>
2014-10-14 03:43:52 +0000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+description = """
+A Python toml parser.
+
+https://github.com/prestontimmons/libtoml
+"""
+
 setup(
     name="libtoml",
     version="1.0",
-    long_description=__doc__,
+    author="Preston Timmons",
+    author_email="prestontimmons@gmail.com",
+    url="https://github.com/prestontimmons/libtoml",
+    description=description,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
